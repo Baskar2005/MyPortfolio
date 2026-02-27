b7cc676e3e30c224be70247688ec9e752ada57f3
baskar2005 <gsarathamgunaskar@gmail.com>
2026-02-27 19:10:45 +0530

    updated portpolio with project url mapp

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-// Optional future animations
-console.log("Portfolio Loaded Successfully");
+const reveals = document.querySelectorAll('.reveal');
+
+function revealOnScroll() {
+  reveals.forEach(element => {
+    const windowHeight = window.innerHeight;
+    const elementTop = element.getBoundingClientRect().top;
+
+    if (elementTop < windowHeight - 100) {
+      element.classList.add('active');
+    }
+  });
+}
+
+window.addEventListener('scroll', revealOnScroll);
